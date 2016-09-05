623dc7b60ff3ae417bef3b8618ca8cfb4fad69a3
Miriam Leon <miriam@cdb51-085mr.anat.ucl.ac.uk>
2016-09-05 23:39:19 +0100

    working on corrections all round

--- chapters/chapterABCFlow/images/acceptance_rate.R
+++ chapters/chapterABCFlow/images/acceptance_rate.R
@@ -1,22 +1,22 @@
 
 
 
-ATc_accept <- c(0.25, 0.111111111111, 0.0555555555556, 0.0263157894737, 0.0166666666667, 0.00847457627119, 0.00325732899023, 0.00227790432802)
-IPTG_accept <-c(0.333333333333, 0.166666666667, 0.0625, 0.0526315789474, 0.0263157894737, 0.0217391304348, 0.008, 0.00787401574803)
-population_Atc <-c(1,2,3,4,5,6,7,8)
-population_iptg <-c(1,2,3,4,5,6,7,8)
+#ATc_accept <- c(0.25, 0.1, 0.0526315789474, 0.0263157894737, 0.0153846153846, 0.0140845070423, 0.00588235294118, 0.00324675324675, 0.00297619047619, 0.00143266475645, 0.00135317997294, 0.0020202020202)
+IPTG_accept <-c(0.333333333333, 0.142857142857, 0.0666666666667, 0.0526315789474, 0.0277777777778, 0.0103092783505, 0.00381679389313, 0.00258397932817, 0.00120048019208, 0.00109170305677 )
+#population_Atc <-c(1,2,3,4,5,6,7,8,9,10,11,12)
+population_iptg <-c(1,2,3,4,5,6,7,8,9,10)
 
 
 
 
 
 
-plot(population_Atc, ATc_accept, xlim=c(0,10), ylim=c(0,0.4),col='#006837', pch=20, xlab='Population', ylab='Acceptance rate')
-lines(population_Atc, ATc_accept,col='#006837')
-points(population_iptg, IPTG_accept, xlim=c(0,10), ylim=c(0,0.4), col='#a50026', pch=20)
-lines(population_iptg, IPTG_accept, col='#a50026')
-legend(7, 0.35, legend=c("ATc induction", "IPTG induction"),
-       col=c('#006837', "#a50026"), lwd=2, cex=1.2,  box.lty=0)
+plot(population_iptg, IPTG_accept, xlim=c(0,11), ylim=c(0,0.4),col='black', pch=20, xlab='Population', ylab='Acceptance rate')
+lines(population_iptg, IPTG_accept,col='black')
+#points(population_iptg, IPTG_accept, xlim=c(0,10), ylim=c(0,0.4), col='#a50026', pch=20)
+#lines(population_iptg, IPTG_accept, col='#a50026')
+#legend(7, 0.35, legend=c("ATc induction", "IPTG induction"),
+ #      col=c('#006837', "#a50026"), lwd=2, cex=1.2,  box.lty=0)
 
 #abline(h=10, lty=5)
 #text(1,12,"Epsilon = 10")
--- chapters/chapterABCFlow/images/epsilon_progression.R
+++ chapters/chapterABCFlow/images/epsilon_progression.R
@@ -1,15 +1,21 @@
 
-ATc_epsilon <- c(68.47745251, 57.8679862609, 49.2835265802, 41.7978223904, 35.3180173666, 30.4227706464, 26.3459953444, 23.8256874601, 21.6831500945, 19.9485472759)
-IPTG_epsilon <-c(64.60678611, 59.0427268608, 54.0599584625, 50.4772129194, 47.5687027846, 45.0386758047,  42.9565751568,  41.2733335703,  39.1936278926)
-population_Atc <-c(1,2,3,4,5,6,7,8,9,10)
-population_iptg <-c(1,2,3,4,5,6,7,8,9)
+#ATc_epsilon <- c(68.47745251, 57.8679862609, 49.2835265802, 41.7978223904, 35.3180173666, 30.4227706464, 26.3459953444, 23.8256874601, 21.6831500945, 19.9485472759)
+#IPTG_epsilon <-c(64.60678611, 59.0427268608, 54.0599584625, 50.4772129194, 47.5687027846, 45.0386758047,  42.9565751568,  41.2733335703,  39.1936278926)
+#iptg_epsilon <- c( 63.92462642, 57.1087483051,52.2361104386, 48.4806314649, 45.8165448896,43.290029663, 40.9192296046,39.2494877308, 37.7549307929,36.1000235947, 34.3629435195)
 
-plot(population_Atc, ATc_epsilon, xlim=c(0,10), ylim=c(0,80), col='#006837', pch=20, xlab='Population', ylab='Epsilon')
-lines(population_Atc, ATc_epsilon, col='#006837')
-points(population_iptg, IPTG_epsilon, xlim=c(0,10), ylim=c(0,80), col='#a50026', pch=20)
-lines(population_iptg, IPTG_epsilon, col='#a50026')
-legend(7, 80, legend=c("ATc induction", "IPTG induction"),
-       col=c('#006837', "#a50026"), lwd=2, cex=1.2,  box.lty=0)
+
+
+
+ATc_epsilon <- c(67.11078191,57.5181715979,50.5950461836,45.6451351091,40.3261491933,35.5722896805,31.4862641396,28.0634782392,25.935024334,23.8590260266,22.1603147097,20.6989018227,19.2977962768)
+population_Atc <-c(1,2,3,4,5,6,7,8,9,10,11,12,13)
+#population_iptg <-c(1,2,3,4,5,6,7,8,9)
+
+plot(population_Atc, ATc_epsilon, xlim=c(0,13), ylim=c(0,80), col='black', pch=20, xlab='Population', ylab='Epsilon')
+lines(population_Atc, ATc_epsilon, col='black')
+#points(population_iptg, IPTG_epsilon, xlim=c(0,10), ylim=c(0,80), col='#a50026', pch=20)
+#lines(population_iptg, IPTG_epsilon, col='#a50026')
+#legend(7, 80, legend=c("ATc induction", "IPTG induction"),
+#       col=c('#006837', "#a50026"), lwd=2, cex=1.2,  box.lty=0)
 #abline(h=10, lty=5)
 #text(1,12,"Epsilon = 10")
 
